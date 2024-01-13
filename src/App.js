743d5d262fb111212533435d13e57b4e615dfa3c
Nicolas Hovart <hovart.nicolas@gmail.com>
2024-01-13 18:35:38 +0100

    %npm install react-router-dom%

--- src/App.js
+++ src/App.js
@@ -3,20 +3,26 @@ import './App.css'
 import Game from './components/Game'
 import HighScores from './components/HighScores'
 import Footer from './components/Footer'
+import {
+  BrowserRouter as Router
+} from 'react-router-dom'
+
 // import Fireworks from 'react-canvas-confetti/dist/presets/fireworks' //from https://www.npmjs.com/package/react-canvas-confetti#Usage
 
 const App = () => {
   return (
-    <div className={'App'}>
-      <div style={{ textAlign: 'center' }}>
-        <div>
-          <h1 className="gradient-red">Memory</h1>
-          <HighScores />
+    <Router>
+      <div className={'App'}>
+        <div style={{ textAlign: 'center' }}>
+          <div>
+            <h1 className="gradient-red">Memory</h1>
+            <HighScores />
+          </div>
+          <Game />
         </div>
-        <Game />
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </Router>
   )
 }
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import {
+  Link
+} from 'react-router-dom'
 
 const Footer = () => {
   return (
     <>
       <div style={{ display: 'flex', flexDirection: 'column', fontSize: 'small' }}>
-        <a href='https://github.com/NicolasHov/memory_react_fruits'>Codebase</a>
-        {/* <div>Client : <a href="">https://github.com/NicolasHov/memory_react_fruits</a></div>
-            <div>API Highscores : <a href="https://github.com/NicolasHov/memory_fruits_server">https://github.com/NicolasHov/memory_fruits_server</a></div> */}
+        <Link to='https://github.com/NicolasHov/memory_react_fruits'>Codebase</Link>
+        {/* <div>Client : <a href=''>https://github.com/NicolasHov/memory_react_fruits</a></div>
+            <div>API Highscores : <a href='https://github.com/NicolasHov/memory_fruits_server'>https://github.com/NicolasHov/memory_fruits_server</a></div> */}
       </div>
     </>
   )
